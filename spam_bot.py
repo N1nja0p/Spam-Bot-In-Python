26bcc9fb7d43277063b7a4307401011aa8f3cd02
N1nja0p <71812498+N1nja0p@users.noreply.github.com>
2021-01-21 18:46:45 +0530

    Update spam_bot.py

--- spam_bot.py
+++ spam_bot.py
@@ -1,7 +1,9 @@
+#Spam Bot Created By Abhimanyu Sharma A.K.A N1nja0p
 import pyautogui
 import time
 time.sleep(5)
-f=open("bee_movie_script.txt")
-for i in f:
-    pyautogui.typewrite(i)
-    pyautogui.press("enter")
+if __name__ == "__main__":
+    f=open("bee_movie_script.txt")
+    for i in f:
+        pyautogui.typewrite(i)
+        pyautogui.press("enter")
